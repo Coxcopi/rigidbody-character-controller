81c9b31567caad7a996d8fda7ebe83fd7df7cac5
Coxcopi <85553012+Coxcopi@users.noreply.github.com>
2021-12-04 16:10:11 +0100

    Initial commit

---
+++ Character 3/Character.gd
@@ -0,0 +1,2 @@
+extends Spatial
+class_name Character, "res://Visuals/UI/Icons/speed.png"
---
+++ Character 3/RigidBody.gd
@@ -0,0 +1,115 @@
+extends RigidBody
+
+var input = Vector3.ZERO
+
+const SENSIVITIVY = 0.025
+const ACCEL = 0.3
+const JUMP_FORCE = 27.5
+
+const SPRINT_MULTIPLIER = 1.4
+const SPEED = 15
+const SLIDE_SPEED_MULTIPLIER = 2.0
+const SLIDE_DECELL = 0.05
+const CROUCH_SPEED_MULT = 0.5
+
+const DEFAULT_FOV = 80
+
+var current_speed = 0.0
+var current_slide_speed = 0.0
+var current_sprint_mult = 1.0
+
+var floor_object_count : int = 0
+
+var is_sliding : bool = false	# true if the character is sliding or crouching
+var is_on_floor : bool = false
+var angular_rot = 0
+
+onready var cam_pivot = $"Camera Pivot"
+onready var cam = $"Camera Pivot/Camera"
+onready var camera_position_default = $CameraPosition_Default
+onready var camera_position_slide = $CameraPosition_Slide
+onready var default_collider = $CollisionShape_Default
+onready var slide_collider = $CollisionShape_Slide
+onready var groundcheck = $GroundCheck
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	cam_pivot.translation = camera_position_default.translation
+	cam.fov = DEFAULT_FOV
+	
+	slide_collider.disabled = true
+	default_collider.disabled = false
+
+func _input(event):
+	if (event is InputEventMouseMotion):
+		angular_rot = -event.relative.x * SENSIVITIVY
+		cam_pivot.rotate_x(deg2rad(-event.relative.y * SENSIVITIVY))
+
+func _integrate_forces(state):
+	
+	is_on_floor = groundcheck.is_colliding()
+	
+	input.z = -(Input.get_action_strength("up") - Input.get_action_strength("down"))
+	input.x = -(Input.get_action_strength("left") - Input.get_action_strength("right"))
+	
+	input = global_transform.basis.xform(input.normalized())
+	
+	current_speed = SPEED
+	
+	angular_velocity.y = angular_rot
+	
+	current_speed *= current_sprint_mult
+	
+	if (is_on_floor):
+		if (Input.is_action_pressed("sprint")):
+			current_sprint_mult = lerp(current_sprint_mult, SPRINT_MULTIPLIER, 0.5)
+		else:
+			current_sprint_mult = lerp(current_sprint_mult, 1.0, 0.5)
+			
+		if (Input.is_action_pressed("slide")):
+			cam_pivot.translation = lerp(cam_pivot.translation, camera_position_slide.translation, 0.2)
+			
+			if (!is_sliding):	# When the slide is initiated
+				initiate_crouch_slide(current_speed)
+			else:	# While sliding
+				current_slide_speed = lerp(current_slide_speed, SPEED * CROUCH_SPEED_MULT, SLIDE_DECELL)
+				current_speed = current_slide_speed
+		else:
+			cam_pivot.translation = lerp(cam_pivot.translation, camera_position_default.translation, 0.2)
+			if (is_sliding):
+				cancel_slide()
+	else:
+		if (is_sliding):
+			cancel_slide()
+	
+	cam.fov = DEFAULT_FOV + (current_speed / SPEED) * 2
+		
+	linear_velocity.x = lerp(linear_velocity.x, input.x * current_speed, ACCEL)
+	linear_velocity.z = lerp(linear_velocity.z, input.z * current_speed, ACCEL)
+	
+	if (Input.is_action_just_pressed("jump") and is_on_floor):
+		linear_velocity.y = JUMP_FORCE
+	
+	angular_rot = 0
+
+func change_slide_collisions(is_now_sliding : bool) -> void:
+	if (is_now_sliding):
+		slide_collider.disabled = false
+		default_collider.disabled = true
+	else:
+		default_collider.disabled = false
+		slide_collider.disabled = true
+
+# Initiate crouch or slide dependent on input_speed
+func initiate_crouch_slide(input_speed : float) -> void:
+	change_slide_collisions(true)
+	if (input_speed > SPEED):
+		current_slide_speed = input_speed * SLIDE_SPEED_MULTIPLIER
+	else:
+		current_slide_speed = SPEED * CROUCH_SPEED_MULT
+	is_sliding = true
+
+# Stop crouching or sliding
+func cancel_slide() -> void:
+	change_slide_collisions(false)
+	is_sliding = false
